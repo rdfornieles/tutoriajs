e4bcd42c275a595d1f5808e20d1b1fdc0dbb5796
rdfornieles <rdfornieles@gmail.com>
2018-12-13 11:16:57 +0100

    change for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+//función que pinte las paletas de colores en HTML
+//hacer una llamada con FETCH para recoger los datos
+//recoge datos, imágenes y pintamos esos datos en HTML
+
+function init() {
+    fetch('https://raw.githubusercontent.com/Adalab/dorcas-repaso-sprint2/master/data/palettes.json')
+    .then(function(response) {
+        console.log('response',response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.palettes);
+        const palettesArr = data.palettes;
+
+        for (const currentPalettes of palettesArr) {
+            console.log(currentPalettes);
+            const listEl = document.querySelector(".container");
+            listEl.innerHTML += `<ul><li>Nombre: ${currentPalettes.name}</li></ul>`;
+        }
+    })  
+}
+init();
